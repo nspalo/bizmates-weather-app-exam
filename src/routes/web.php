5416b4fb49c74956c698f535c50a63f1bf12fe3b
Noel Palo <noelspalo@gmail.com>
2023-09-07 16:18:37 +0800

    feature/BWA-4-weather-app-ui-development-bootstrap - Initial UI Set up for Bootstrap 5 + Vite

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\WeatherApp\WeatherUpdateController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,7 @@ Route::get(
     '/',
     [WeatherUpdateController::class, 'index']
 )->name('landing-page');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
